f7e2af520c66dae0deb379e88590ab090241a4f9
ItsMeIshan <65131563+ItsMeIshan@users.noreply.github.com>
2024-11-14 18:11:36 +0530

    fix: fixes redirect after sign up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -64,8 +64,11 @@ function SignUpForm() {
   const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
     setIsLoading(true);
     try {
-      await axios.post("/api/sign-up", data);
+      const response = await axios.post("/api/sign-up", data);
       setIsLoading(false);
+      if (response?.config?.url) {
+        router.replace("/sign-in");
+      }
     } catch (error) {
       console.error("Error during sign-up:", error);
       setError("There was a problem with your sign-up. Please try again.");
